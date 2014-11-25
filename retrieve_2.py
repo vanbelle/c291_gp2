2ccfa5f2eafc86056da368b119312fa35dc139b1
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-11-25 14:06:03 -0700

    new files

---
+++ retrieve_2.py
@@ -0,0 +1,8 @@
+#retrieves records with a given key
+import time
+import bsddb3 as bsddb
+
+
+start_time = time.time()
+print('this function retrived %s records' %records)
+print('this function took %s seconds to run' %time.time() - start_time)
