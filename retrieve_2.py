5daaf8b69f5f84b6031f2958ade48f9eb2bb8fac
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-11-25 14:12:24 -0700

    small change

--- retrieve_2.py
+++ retrieve_2.py
@@ -5,4 +5,4 @@ import bsddb3 as bsddb
 
 start_time = time.time()
 print('this function retrived %s records' %records)
-print('this function took %s seconds to run' %time.time() - start_time)
+print('this function took %s seconds to run' %time.time() - start_time) 
