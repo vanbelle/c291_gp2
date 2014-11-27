c5b60260ef3ed9c41713f19f50ead19cd56ba6a5
rcockbur <rcockbur@ualberta.ca>
2014-11-27 15:13:14 -0700

    pop now uses startCommand

--- pop_db.py
+++ pop_db.py
@@ -7,23 +7,26 @@ import random
 class pop_db(object):
     def __init__(self, startCommand):
         super(pop_db, self).__init__()
-        DA_FILE = "/tmp/vanbelle_db/btree.db"
-        DC_FILE = "/tmp/vanbelle_db/hash.db"
         DB_SIZE = 10
         SEED = 10000000
-        try:
-            db = bsddb.btopen(DA_FILE, "w")
-        except:
-            print("Btree doesn't exist, creating a new one")
-            db = bsddb.btopen(DA_FILE, "c")
-        
-        try:
-            dc = bsddb.hashopen(DC_FILE, "w")
-        except:
-            print("Hash table doesn't exist, creating a new one")
-            dc = bsddb.hashopen(DC_FILE, "c")        
         random.seed(SEED)
 
+        if startCommand == "btree":
+            DA_FILE = "/tmp/vanbelle_db/btree.db"
+            try:
+                db = bsddb.btopen(DA_FILE, "w")
+            except:
+                print("Btree doesn't exist, creating a new one")
+                db = bsddb.btopen(DA_FILE, "c")
+
+        elif startCommand == "hash":
+            DC_FILE = "/tmp/vanbelle_db/hash.db"
+            try:
+                dc = bsddb.hashopen(DC_FILE, "w")
+            except:
+                print("Hash table doesn't exist, creating a new one")
+                dc = bsddb.hashopen(DC_FILE, "c")          
+        
         for index in range(DB_SIZE):
             krng = 64 + self.get_random()
             key = ""
@@ -38,16 +41,24 @@ class pop_db(object):
             print ("")
             key = key.encode(encoding='UTF-8')
             value = value.encode(encoding='UTF-8')
-            db[key] = value
-            dc[key] = value
-        try:
-            db.close()
-        except Exception as e:
-            print (e)
-        try:
-            dc.close()
-        except Exception as e:
-            print (e)        
+            
+            if startCommand == "btree":
+                db[key] = value
+
+            elif startCommand == "hash":
+                dc[key] = value
+                
+        if startCommand == "btree":
+            try:
+                db.close()
+            except Exception as e:
+                print (e)
+
+        elif startCommand == "hash":
+            try:
+                dc.close()
+            except Exception as e:
+                print (e)
     
     def get_random(self):
         return random.randint(0, 63)
