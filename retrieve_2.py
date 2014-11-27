7f196067486f68e026d2264c1e8cdc2b6509659c
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-11-27 14:07:27 -0700

    added stuff

--- retrieve_2.py
+++ retrieve_2.py
@@ -3,9 +3,23 @@ import time
 import bsddb3 as bsddb
 
 class retrieve_2(object):
-    def __init(self, table, date):
+    def __init(self, table, data):
         self.start_time = time.time()
+        self.keys = []
         
+        if type_option == 1:
+            self.file = "/tmp/vanbelle_db/btree.db"
+            self.db = bsddb.btopen(self.file,'r')
+        elif type_option == 2:
+            self.file = "/tmp/vanbelle_db/hash.db"
+            self.db = bsddb.hashopen(self.file,'r')    
+        
+        self.cursor = self.db.cursor()    
+        self.rec = self.cursor.first()
+        while self.rec:
+            if self.rec[1] == data:
+                self.keys.append(self.rec)
+            self.rec = cursor.next()
         
         try:
             self.db.close()
--- retrieve_3.py
+++ retrieve_3.py
@@ -5,10 +5,26 @@ import bsddb3 as bsddb
 class retrieve_3(object):
     def __init__(self,table,low,high):
         self.start_time = time.time()
+        self.keys = []
         
+        if type_option == 1:
+            self.file = "/tmp/vanbelle_db/btree.db"
+            self.db = bsddb.btopen(self.file,'r')
+        elif type_option == 2:
+            self.file = "/tmp/vanbelle_db/hash.db"
+            self.db = bsddb.hashopen(self.file,'r')    
+        
+        self.cursor = self.db.cursor()    
+        self.rec = self.cursor.first()
+        while self.rec:
+            if self.rec[0] >= low && self.rec[0] <= high:
+                self.keys.append(self.rec)
+            self.rec = cursor.next() 
+            
         try:
             self.db.close()
         except Exception as e:
             print (e)        
         print('this function retrived %s records' %len(self.records))
         print('this function took %s seconds to run' %time.time() - self.start_time)
+        return self.keys
