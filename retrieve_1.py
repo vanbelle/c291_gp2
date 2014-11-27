c2abe834d6453eabf108afde78f00706e2ac83ef
Megan Sumner <msumner@cs.ualberta.ca>
2014-11-27 15:39:51 -0700

    This is working

--- retrieve_1.py
+++ retrieve_1.py
@@ -6,20 +6,19 @@ class retrieve_1(object):
     def __init__(self,type_option,key):
         super(retrieve_1,self).__init__()
         self.start_time = time.time()
-        
+        self.value = None
         if type_option == "btree":
             self.file = "/tmp/vanbelle_db/btree.db"
             self.db = bsddb.btopen(self.file,'r')
-            print(self.db[1])
+            self.value = (self.db[key.encode(encoding='UTF-8')])
+            
         elif type_option == "hash":
             self.file = "/tmp/vanbelle_db/hash.db"
             self.db = bsddb.hashopen(self.file,'r')
             
-        self.db = bsddb.btopen(self.file,'r')
-        self.value = self.db[key]
         try:
             self.db.close()
         except Exception as e:
             print (e)        
-        print('this function took %s seconds to run' %time.time() - self.start_time)
-        return self.value
+        print(self.value.decode(encoding='UTF-8'))
+        print('this function took %s seconds to run' %(time.time() - self.start_time))
