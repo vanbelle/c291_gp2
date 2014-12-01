ba6f25c3183aa5ffdf11d1697951e5759b923acf
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-12-01 14:43:03 -0700

    fixed return problems

--- retrieve_1.py
+++ retrieve_1.py
@@ -9,17 +9,31 @@ class retrieve_1(object):
         self.value = None
         if type_option == "btree":
             self.file = "/tmp/vanbelle_db/btree.db"
-            self.db = bsddb.btopen(self.file,'r')            
+            self.db = bsddb.btopen(self.file,'r')  
+            try:    
+                self.value = (self.db[key.encode(encoding='UTF-8')]) 
+                try:
+                    self.db.close()
+                except Exception as e:
+                    print (e)   
+                self.value =  self.value.decode(encoding='UTF-8')    
+                print('this function took %s microseconds to run' %((time.time() - sstart_time)*1000000))    
+            except:
+                print('invalid key')       
         elif type_option == "hash":
             self.file = "/tmp/vanbelle_db/hash.db"
             self.db = bsddb.hashopen(self.file,'r')
-        try:    
-            self.value = (self.db[key.encode(encoding='UTF-8')]) 
-            try:
-                self.db.close()
-            except Exception as e:
-                print (e)   
-                self.value =  self.value.decode(encoding='UTF-8')    
+            try:    
+                self.value = (self.db[key.encode(encoding='UTF-8')]) 
+                try:
+                    self.db.close()
+                except Exception as e:
+                    print (e)   
+                    self.value =  self.value.decode(encoding='UTF-8')    
                 print('this function took %s microseconds to run' %((time.time() - sstart_time)*1000000))
-        except:
-            print('invalid key')
+            except:
+                print('invalid key')
+            
+        elif type_option == 'indexfile':
+            pass
+        
