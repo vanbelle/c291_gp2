15ab3412bfd3ccf49cc4f22cd849ac8792304b31
Megan Sumner <msumner@cs.ualberta.ca>
2014-12-02 13:06:09 -0700

    destroyed things :D

--- destroy_db.py
+++ destroy_db.py
@@ -1,11 +1,11 @@
 #destroys existing databases
 import bsddb3 as bsddb
-
+import os
 class destroy_db(object):
     def __init__(self):
-        DA_FILE = "/tmp/vanbelle_db/btree.db"
-        DB_FILE = "/tmp/vanbelle_db/hash.db"
-        DC_FILE = "/tmp/vanbelle_db/index.db"
-        bsddb.db.remove(DA_FILE)   
-        bsddb.db.remove(DB_FILE)
-        bsddb.db.remove(DC_FILE)
+        DA_FILE = "/tmp/vanbelle_db"
+        try:
+            os.removedirs(DA_FILE)
+        except:
+            print("Nothing to destroy")
+       
