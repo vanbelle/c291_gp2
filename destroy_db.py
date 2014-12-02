e98b5721bc83e200d910e3f1f8d5aa397572833a
Sarah Van Belleghem <vanbelle@cs.ualberta.ca>
2014-12-01 19:23:50 -0700

    why not

--- destroy_db.py
+++ destroy_db.py
@@ -6,6 +6,6 @@ class destroy_db(object):
         DA_FILE = "/tmp/vanbelle_db/btree.db"
         DB_FILE = "/tmp/vanbelle_db/hash.db"
         DC_FILE = "/tmp/vanbelle_db/index.db"
-        bsddb.remove(DA_FILE)   
+        bsddb.db.remove(DA_FILE)   
         bsddb.db.remove(DB_FILE)
         bsddb.db.remove(DC_FILE)
